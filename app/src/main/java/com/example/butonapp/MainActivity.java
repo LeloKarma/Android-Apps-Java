d0eb21e5aa4c4f2af5aa7fbd18fa2a4c8638e555
Lelokarma <sharonlelo6@gmail.com>
2024-05-11 07:20:12 +0200

    simple android app

--- app/src/main/java/com/example/butonapp/MainActivity.java
+++ app/src/main/java/com/example/butonapp/MainActivity.java
@@ -5,42 +5,46 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private LinearLayout mainLayout; // Reference to the main layout
+    private RelativeLayout bgElement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize views
-        Button button = findViewById(R.id.button);
-        mainLayout = findViewById(R.id.main_layout); // Assuming 'main_layout' is the ID of your main layout
+        // Initialize the RelativeLayout
+        bgElement = findViewById(R.id.activity_main);
 
-        // Set button click listener
+        //optional
+        bgElement.setBackgroundColor(Color.WHITE);
+
+        // Call method to set button click listener
+        myButtonListenerMethod();
+    }
+
+    // Method to set button click listener
+    public void myButtonListenerMethod() {
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Toggle the background color of the main layout
-                toggleBackgroundColor();
+                // Get the current background color of the RelativeLayout
+                int color = ((ColorDrawable) bgElement.getBackground()).getColor();
+
+                // Toggle between RED and BLUE background colors
+                if (color == Color.RED) {
+                    bgElement.setBackgroundColor(Color.BLUE);
+                    button.setText("Blue");
+                } else {
+                    bgElement.setBackgroundColor(Color.RED);
+                    button.setText("Red");
+                }
             }
         });
-    }
-
-    // Method to toggle the background color of the main layout
-    private void toggleBackgroundColor() {
-        // Get the current background color of the main layout
-        int currentColor = ((ColorDrawable) mainLayout.getBackground()).getColor();
-
-
-        if (currentColor == Color.RED) {
-            mainLayout.setBackgroundColor(Color.BLUE);
-        } else {
-            mainLayout.setBackgroundColor(Color.RED);
-        }
     }
 }
